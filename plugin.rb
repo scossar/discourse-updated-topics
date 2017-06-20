7732801cc5d821abf650df7c3859baeb5e70d212
scossar <scossar3@gmail.com>
2017-06-19 17:03:29 -0700

    Remove mismatched embed_urls outside of main query

--- plugin.rb
+++ plugin.rb
@@ -20,38 +20,48 @@ after_initialize do
   class DiscourseUpdatedTopics::UpdatedTopicsController < ::ApplicationController
 
     # Returns Discourse Topic data for topics that have been updated between a
-    # given start time and the current time. The embed_url is used on WordPress
-    # to match a Topic to a WordPress post. Ideally, the WordPress post_id would
-    # be saved on Discourse and returned with this data. If the WordPress post_id
-    # is added as a property to the Discourse topic, the plugin will still need to
-    # return the embed_url so that it can be used with Topics that were created
-    # before the property was added to Discourse Topics. Possibly, a 'legacy' option
-    # could be added to the plugin, so that when the plugin is used on new Discourse/WordPress
-    # installations, the join to the TopicEmbed table could be left out.
-    # For multisite installations, the topic_embed will need to be required. To
-    # only pull data for posts published on a specific WordPress subsite, the site_url
-    # is sent as a parameter. If this is too inefficient, it would be possible to
-    # return all data to each subsite, and parse the data on WordPress.
+    # given start time and the current time.
+    #
+    # The Discourse api_key, api_username, and WordPress site_url are passed as query
+    # params from WordPress.
+    # The embed_url is used on WordPress to match a Topic to a WordPress post.
+    # The site_url is used to match topics to WordPress subsites on multisite
+    # Wordpress installations.
+    # If connected to a WordPress multisite setup, this function could be called
+    # as ofter as once every 10 minutes by each site on the network. Instead of
+    # selecting topic_embeds that match the site_url in the topic_data query, I'm
+    # removing them from the result of the query after it's been made. The reason
+    # for doing it this way is so that the result of the topic_data query can be cached.
+
     def topic_data
-      site_url = CGI.unescape(params[:site_url])
+      site_url = params[:site_url].present? ? CGI.unescape(params[:site_url]) : nil
       since = params[:sync_period].to_i.minutes
       time_range = (Time.current - since)..Time.current
 
       topics_data = Topic.joins(:topic_embed)
                         .where(updated_at: time_range)
-                        .where("topic_embeds.embed_url LIKE ?", "#{site_url}/%")
+                        .limit(30)
                         .pluck(:id, :title, :posts_count, :embed_url)
                         .map {|t| {id: t[0], title: t[1], comment_count: t[2] - 1, embed_url: t[3]}}
 
+      # Possibly useful for dealing with WordPress multisite installations. I think it's
+      # going to be possible to remove this.
+      if site_url
+        topics_data.delete_if do |data_set|
+          !data_set[:embed_url].starts_with?(site_url + '/')
+        end
+      end
+
       render json: topics_data
     end
+
   end
 
   DiscourseUpdatedTopics::Engine.routes.draw do
-    get 'topic-data/:sync_period' => 'updated_topics#topic_data', constraints: { sync_period: /\d+/, format: 'json' }
+    get 'topic-data/:sync_period' => 'updated_topics#topic_data', constraints: {sync_period: /\d+/, format: 'json'}
   end
 
   Discourse::Application.routes.append do
-    mount ::DiscourseUpdatedTopics::Engine, at: 'discourse-updated-topics', constraints: AdminConstraint.new
+    mount ::DiscourseUpdatedTopics::Engine, at: 'discourse-updated-topics'
   end
 end
